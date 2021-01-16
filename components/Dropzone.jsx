bfd719b4526507a3489779b00552ebec82640a96
Jack Sari <janasarii@gmail.com>
2021-01-16 10:50:25 -0500

    19-01-2020

--- components/Dropzone.jsx
+++ components/Dropzone.jsx
@@ -1,11 +1,53 @@
 import React,{ useState, useCallback } from 'react';
 import { useDropzone } from 'react-dropzone'
+import clienteAxios from "../config/axios";
 
 
 function Dropzone() {
-    return (
-        <div>
 
+    const onDrop = useCallback(async (acceptedFiles) => {
+        console.log('soltando archivo',acceptedFiles)
+
+        //crear un form data
+        const formData = new FormData()
+        formData.append('archivo', acceptedFiles[0])
+
+        const resp = await clienteAxios.post('/api/archivos', formData)
+        console.log('res', resp)
+    },[])
+
+
+    //Extraer  contenido del dropzone
+    const { getRootProps, getInputProps, isDragActive, acceptedFiles } = useDropzone({onDrop});
+
+
+    const archivos = acceptedFiles.map((archivo,index) => (
+        <li key={index} className="bg-white flex-1 p-3 mb-4 shadow-lg rounded">
+            <p className="font-bold text-xl">{archivo.path}</p>
+            <p className="text-sm text-gray-500">{(archivo.size/Math.pow(1024,2)).toFixed(3)} MB</p>
+        </li>
+    ))
+
+    return (
+        <div className="md:flex-1 mb-3 mx-2 mt-16 lg:mt-0 flex flex-col justify-center items-center border-dashed border-gray-400 border-2 bg-gray-100">
+
+            {acceptedFiles.length > 0 ? (
+                <ul className="p-3">
+                    {archivos}
+                </ul>
+            ): (
+                <div {...getRootProps({ className: "dropzone w-full py-32" })}>
+                    <input type="text" className="h-100" {...getInputProps()}/>
+                    {
+                        isDragActive ? <p className="text-2xl text-center text-gray-600">Suelta el archivo</p>: (
+                            <div className="text-center">
+                                <p className="text-2xl text-center text-gray-600 ">Selecciona un archivo y arrastralo aquí</p>
+                                <button className="mx-10 px-5 bg-blue-700 py-3 rounded-lg text-white my-10 hover:bg-blue-800" type="button">Selecciona archivos para subir</button>
+                            </div>
+                        )
+                    }
+                </div>
+            )}
         </div>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Layout from "../components/Layout";
 import authContext from "../context/auth/authContext";
 import React,{useContext,useEffect} from 'react'
 import Link from 'next/link'
+import Dropzone from "../components/Dropzone";
 
 export default function Home() {
     //Extrar el usuario autenticado del storage
@@ -18,9 +19,7 @@ export default function Home() {
     <Layout>
       <div className="md:w-4/5 xl:w-3/5 mx-auto mb-32">
             <div className="lg:flex md:shadow-lg p-5 bg-white rounded-lg py-10">
-                <div className="md:flex-1 mb-3 mx-2 mt-16 lg:mt-0">
-                    <p>Dropzone aquí</p>
-                </div>
+                <Dropzone/>
                 <div className="md:flex-1 mb-3 mx-2 mt-16 lg:mt-0">
                     <h2 className="text-4xl font-sans font-bold text-gray-800 my-4">Compartir archivos de forma sencilla y privada</h2>
                     <p className="text-lg leading-loose my-2">
